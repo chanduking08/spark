33d2a84d9f539c4db4d180600475666a317e8c04
Thangaraj Dialedin <thangaraj@dialedin.in>
2021-05-26 20:40:06 +0530

    php mailer and responsive fix

--- submit.php
+++ submit.php
@@ -23,5 +23,36 @@
     }
 
   $conn->close();
+  ?>
+  <p class="text-white">Thanks for submitting. You will receive a welcome email shortly!</p>
+
+  <?php
+  // Start with PHPMailer class
+  use PHPMailer\PHPMailer\PHPMailer;
+  require_once './vendor/autoload.php';
+  // create a new object
+  $mail = new PHPMailer();
+  // configure an SMTP
+  $mail->isSMTP();
+  $mail->Host = 'smtp.mailtrap.io';
+  $mail->SMTPAuth = true;
+  $mail->Username = '6491518d1794f4';
+  $mail->Password = '1695a43d906eed';
+  $mail->SMTPSecure = 'tls';
+  $mail->Port = 2525;
+
+  $mail->setFrom('thangaraj120.com@gmail.com', 'Your Pledge');
+  $mail->addAddress($email, 'Me');
+  $mail->Subject = 'Thanks for Pledge with us!';
+  // Set HTML
+  $mail->isHTML(TRUE);
+  $mail->Body = '<html>Hi there, we are happy to <br>confirm your pledge.</html>';
+  $mail->AltBody = 'Hi there, we are happy to confirm your pledge.';
+  // send the message
+  $mail->send();
+  if(!$mail->send()){
+      echo 'Message could not be sent.';
+      echo 'Mailer Error: ' . $mail->ErrorInfo;
+  }
+
 ?>
-<p class="text-white">Thanks for submitting. You will receive a welcome email shortly!</p>
